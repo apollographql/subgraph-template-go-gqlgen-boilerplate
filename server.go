bf05ebde753937c2c832bb6175473bd3f0c5e29f
Dylan Anthony <dbanty@users.noreply.github.com>
2023-02-07 11:57:49 -0700

    feat: Add `Router-Authorization` header support

--- server.go
+++ server.go
@@ -21,13 +21,16 @@ func main() {
 	}
 	router := chi.NewRouter()
 
-	// Add CORS middleware around every request
-	// See https://github.com/rs/cors for full option listing
 	router.Use(cors.New(cors.Options{
 		AllowedOrigins:   []string{"https://studio.apollographql.com"},
 		AllowCredentials: true,
 		AllowedHeaders:   []string{"*"},
 	}).Handler)
+
+	routerSecret, isSet := os.LookupEnv("ROUTER_SECRET")
+	if isSet {
+		router.Use(checkRouterAuth(routerSecret))
+	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
@@ -35,4 +38,18 @@ func main() {
 
 	log.Printf("Explore with \"https://studio.apollographql.com/sandbox/explorer?endpoint=http://localhost:" + port + "\"")
 	log.Fatal(http.ListenAndServe(":"+port, router))
+}
+
+// Produce a middleware to check that the `Router-Authorization` header is set and matches routerSecret
+func checkRouterAuth(routerSecret string) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			header := r.Header.Get("Router-Authorization")
+			if header != routerSecret {
+				http.Error(w, "Authorization required", http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
 }
